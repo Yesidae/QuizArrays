a0a9f8a7e40f2954f60f4a6ec9a79442a1559f5f
salan404 <salan404@SALAN404-15>
2023-03-17 19:41:04 -0500

    Add project files.

---
+++ QuizArrays.Logic/OneDimension.cs
@@ -0,0 +1,62 @@
+﻿namespace QuizArrays.Logic
+{
+    public class OneDimension
+    {
+
+        #region Fields
+        private int[] _array; // inicia el arreglo
+        private int _top; // variable para el tope fisico
+        #endregion
+
+        #region Properties
+        private bool IsFull => _top == N; // lleno
+
+        private bool IsEmpty => _top == 0; // vacio
+
+        public int N { get; } // tamaño de las posiciones con algo en ellas
+        #endregion
+
+        #region Methods
+        public OneDimension(int n) // constructor
+        {
+            N = n;
+            _array = new int[n];
+            _top = 0;
+        }
+
+        public void Add(int number) // añade el numero al estar lleno indica que esta lleno
+        {
+            if (IsFull)
+            {
+                throw new Exception("The array is full.");
+            }
+
+            _array[_top] = number;
+            _top++;
+        }
+
+        public override string ToString()
+        {
+            if (IsEmpty)
+            {
+                return "Empty array.";
+            }
+
+            string output = string.Empty;
+            int count = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                output += $"{_array[i]}\t";
+                count++;
+                if (count > 9)
+                {
+                    count = 0;
+                    output += "\n";
+                }
+            }
+            return output;
+        }
+        #endregion
+
+    }
+}
---
+++ QuizArrays.Logic/TwoDimensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizArrays.Logic
+{
+    public class TwoDimensions
+    {
+        private int[,] _array; // inicia el arreglo de 2 dimensiones
+
+        public int M { get; } // propiedad M que solo deja leer
+        public int N { get; } // propiedad N que solo deja leer
+
+        public TwoDimensions(int n, int m) // Const que dimensiona el arreglo
+        {
+            N = n;
+            M = m;
+            _array = new int[n,m];
+        }
+
+        public void Fill() // metodo fill
+        {
+            var random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    _array[i, j] = random.Next(1, 100); 
+                }
+            }
+        }
+
+        public ToOneDimension() // convierte 2 dimensiones en una
+        {
+            int[] newArray;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    int aux = 0;
+                    aux = _array[i, j] ;
+                    newArray[i] = aux;
+                }
+            }
+
+            return newArray;
+        }
+
+        public override string ToString() // convierte a string
+        {
+            string output = string.Empty;
+            int count = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    output += $"{_array[i,j]}\t";
+                    count++;
+                    if (count > 9)
+                    {
+                        count = 0;
+                        output += "\n";
+                    } 
+                }
+            }
+            return output;
+        }
+    }
+}
